eee163e50b02bc9d55c0f276b1aca89589666878
Michelle Calderon <calderonpedraza.lauramichelle@gmail.com>
2023-10-02 18:27:02 -0500

    Se agrega header y upcoming movies

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** navbar variables*/
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElems = [ overlay,navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElems.length; i++) {
+  navElems[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+  });
+
+}
+
+
+
+/**header sticky*/
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
+
+});

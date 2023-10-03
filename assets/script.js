50f79e186b80960c9b822dd07cb2a93b74ba6652
Michelle Calderon <calderonpedraza.lauramichelle@gmail.com>
2023-10-03 17:48:12 -0500

    Se agregan imagenes y se hace responsiva

--- assets/script.js
+++ assets/script.js
@@ -29,3 +29,14 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
 
 });
+
+
+/**go to top*/
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
+
+});
